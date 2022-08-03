375a3af8a844d5e2fe721f7a0f80c24d88b1f85c
Jorge Reyes <aeroestadistica@gmail.com>
2022-08-03 14:33:21 -0500

    Eliminar vacantes

--- app/Http/Controllers/VacanteController.php
+++ app/Http/Controllers/VacanteController.php
@@ -24,19 +24,7 @@ class VacanteController extends Controller
      */
     public function create()
     {
-        //
         return view('vacantes.create');
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
     }
 
     /**
@@ -63,28 +51,5 @@ class VacanteController extends Controller
         return view('vacantes.edit', [
             'vacante' => $vacante,
         ]);
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        //
     }
 }
--- resources/views/livewire/mostrar-vacantes.blade.php
+++ resources/views/livewire/mostrar-vacantes.blade.php
@@ -1,37 +1,68 @@
-<div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-    @forelse ($vacantes as $vacante)
-        <div class="p-6 bg-white border-b border-gray-200 md:flex md:justify-between md:items-center">
-            <div class="space-y-3">
-                <a href="#" class="text-xl font-bold">
-                    {{ $vacante->titulo }}
-                </a>
-                <p class="text-sm text-gray-600 font-bold">
-                    {{ $vacante->empresa }}
-                </p>
-                <p class="text-sm text-gray-500">
-                    Último día: {{ $vacante->ultimo_dia->format('d/m/Y') }}
-                </p>
+<div>
+    <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+        @forelse ($vacantes as $vacante)
+            <div class="p-6 bg-white border-b border-gray-200 md:flex md:justify-between md:items-center">
+                <div class="space-y-3">
+                    <a href="#" class="text-xl font-bold">
+                        {{ $vacante->titulo }}
+                    </a>
+                    <p class="text-sm text-gray-600 font-bold">
+                        {{ $vacante->empresa }}
+                    </p>
+                    <p class="text-sm text-gray-500">
+                        Último día: {{ $vacante->ultimo_dia->format('d/m/Y') }}
+                    </p>
+                </div>
+                <div class="flex flex-col items-stretch md:flex-row gap-3 mt-5 md:mt-0">
+                    <a href="#"
+                        class="uppercase bg-slate-800 py-2 px-4 rounded-lg text-white text-xs font-bold text-center">
+                        Candidatos
+                    </a>
+                    <a href="{{ route('vacantes.edit', $vacante->id) }}"
+                        class="uppercase bg-blue-600 py-2 px-4 rounded-lg text-white text-xs font-bold text-center">
+                        Editar
+                    </a>
+                    <button wire:click="$emit('mostrarAlerta', {{ $vacante->id }})"
+                        class="uppercase bg-red-600 py-2 px-4 rounded-lg text-white text-xs font-bold text-center">
+                        Eliminar
+                    </button>
+                </div>
             </div>
-            <div class="flex flex-col items-stretch md:flex-row gap-3 mt-5 md:mt-0">
-                <a href="#"
-                    class="uppercase bg-slate-800 py-2 px-4 rounded-lg text-white text-xs font-bold text-center">
-                    Candidatos
-                </a>
-                <a href="{{ route('vacantes.edit', $vacante->id) }}"
-                    class="uppercase bg-blue-600 py-2 px-4 rounded-lg text-white text-xs font-bold text-center">
-                    Editar
-                </a>
-                <a href="#"
-                    class="uppercase bg-red-600 py-2 px-4 rounded-lg text-white text-xs font-bold text-center">
-                    Eliminar
-                </a>
-            </div>
-        </div>
-    @empty
-        <p class="p-3 text-center text-sm text-gray-600">No hay vacantes que mostrar.</p>
-    @endforelse
+        @empty
+            <p class="p-3 text-center text-sm text-gray-600">No hay vacantes que mostrar.</p>
+        @endforelse
+    </div>
+
+    <div class="mt-10">
+        {{ $vacantes->links() }}
+    </div>
 </div>
 
-<div class="mt-10">
-    {{ $vacantes->links() }}
-</div>
+
+@push('scripts')
+    <script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+
+    <script>
+        Livewire.on('mostrarAlerta', vacanteId => {
+            Swal.fire({
+                title: '¿Eliminar Vacante?',
+                text: "Una vacante eliminada no se puede recuperar",
+                icon: 'warning',
+                showCancelButton: true,
+                confirmButtonColor: '#3085d6',
+                cancelButtonColor: '#d33',
+                confirmButtonText: 'Si, eliminar!',
+                cancelButtonText: 'Cancelar',
+            }).then((result) => {
+                if (result.isConfirmed) {
+                    Livewire.emit('eliminarVacante', vacanteId);
+                    Swal.fire(
+                        'Se eliminó la vacante!',
+                        'Eliminado correctament.',
+                        'success'
+                    )
+                }
+            })
+        })
+    </script>
+@endpush
